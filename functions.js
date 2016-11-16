6c8c0b089660ddcc76c2409cda4cdd8bf2e1620c
Kshitiz Tiwari <tiwarikshitiz@gmail.com>
2016-11-16 11:17:49 +0545

    Header size decrease, footer padding, square logo added

---
+++ functions.js
@@ -0,0 +1,53 @@
+_ = {};
+
+	_.throttle = function(func, wait, options) {
+		var context, args, result;
+		var timeout = null;
+		var previous = 0;
+		if (!options) options = {};
+		var later = function() {
+			previous = options.leading === false ? 0 : new Date().getTime();
+			timeout = null;
+			result = func.apply(context, args);
+			if (!timeout) context = args = null;
+		};
+		return function() {
+			var now = new Date().getTime();
+			if (!previous && options.leading === false) previous = now;
+			var remaining = wait - (now - previous);
+			context = this;
+			args = arguments;
+			if (remaining <= 0 || remaining > wait) {
+				if (timeout) {
+					clearTimeout(timeout);
+					timeout = null;
+				}
+				previous = now;
+				result = func.apply(context, args);
+				if (!timeout) context = args = null;
+			} else if (!timeout && options.trailing !== false) {
+				timeout = setTimeout(later, remaining);
+			}
+			return result;
+		};
+	};
+
+$( document ).ready( function(){
+	
+	$mainHeader = $('#main-header');
+	$window = $(window);
+
+	var sticky_header = function(){
+		var scroll_pos = $window.scrollTop();
+
+		if( scroll_pos > 80 ){
+			$mainHeader.addClass( 'sticky' );
+		} else {
+			$mainHeader.removeClass( 'sticky' );
+		}
+	}
+
+	sticky_header();
+
+	$window.scroll( _.throttle( sticky_header, 200)  ); 
+} );
